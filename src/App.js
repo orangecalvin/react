c4d833f2fd90d1283fb56d513c34e568f4a8b87c
kyu <kncljmmmm@gmail.com>
2023-05-15 00:14:34 +0900

    리엑트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕 나는 리엑트  실시간  Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
