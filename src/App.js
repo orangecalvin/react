43afe0d42396805423e962dcc5109c43cff08156
kyu <kncljmmmm@gmail.com>
2023-05-25 23:29:19 +0900

    리엑트 ~1-7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* eslint-disable */ 
+/* eslint-disable */
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,13 +6,10 @@ import { useState } from 'react';
 
 function App() {
   let post = '수유 우동 맛집';
-  let [글제목, 글제목변경] = useState(['남자 코트 추천','수유 우동 맛집','파이썬독학' ]);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '수유 우동 맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
 
-  let 함수 = () => 따봉변경(따봉+1);
-  
-
-
+  let 함수 = () => 따봉변경(따봉 + 1);
 
 
 
@@ -22,28 +19,45 @@ function App() {
         <h4>리엑트 블로그</h4>
       </div>
 
-          <button>가나다순 정렬</button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy)
+      }}> 정렬버튼 </button>
 
-          <button onClick={() => {
-            let copy = [...글제목];
-            copy[0] = '여자코트 추천';
-            글제목변경(copy);
-            }}>글수정</button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
 
       <div className="list">
-        <h4>{ 글제목[0] } <span onClick={ 함수 }>👍</span> { 따봉 } </h4>
+        <h4>{글제목[0]} <span onClick={함수}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[1] }</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal/>
+
+
     </div>
   );
 }
 
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
